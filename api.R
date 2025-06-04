e5724ad10bab534e706cb44d1c72eb0f15d96e58
Carlos Pérez <carlos@gmail.com>
2025-06-04 10:32:54 +0200

    subo de nuevo

--- api.R
+++ api.R
@@ -1,5 +1,5 @@
 library(plumber)
-#* @get  /histogram
+#* @get /hist
 #* @param si_no
 #* @apiDescription para ver el histograma
 #* @apiTitle histograma
@@ -9,6 +9,5 @@ function(si_no){
   return(ifelse(si_no=="si",
          hist(sapply(datos$Minutes,function(x) log(x))),
          "no voy a devolver nada"))
-  
 }
 
